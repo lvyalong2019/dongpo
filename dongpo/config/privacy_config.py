b46ef12c451758abcad2b51a5c2c2ef00bcf95af
lvyalong <694474349@qq.com>
2024-04-23 12:13:46 +0800

    新增配置

--- dongpo/config/privacy_config.py
+++ dongpo/config/privacy_config.py
@@ -1,14 +1,14 @@
 # 隐私配置，暂不同步到github
 
 # 用户名、密码
-username = 'lv_ya_long'
-pwd = 'tai_ku_la'
+username = ''
+pwd = ''
 
 # 我的飞书群机器人Webhook地址
-my_feishu_url = 'https://open.feishu.cn/open-apis/bot/v2/hook/4f1fd6f9-749c-4909-9c18-a1805fa00f92'
+my_feishu_url = ''
 
 # 文杰的飞书群机器人Webhook地址
-wenjie_feishu_url = 'https://open.feishu.cn/open-apis/bot/v2/hook/74b3a942-a943-4e31-a95c-1bf9f19e7046'
+wenjie_feishu_url = ''
 
 # 通用飞书群机器人Webhook地址
-common_feishu_url = 'https://open.feishu.cn/open-apis/bot/v2/hook/591869fc-15c8-4e86-a110-649b40492438'
+common_feishu_url = ''
